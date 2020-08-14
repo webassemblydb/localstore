178504d81d1e3f127e5762cd0378e22ce8607df1
huaian <nopains@126.com>
2020-08-15 06:33:43 +0800

    【重构】函数放到回调

--- indexedDB.js
+++ indexedDB.js
@@ -4,49 +4,55 @@ var request = window.indexedDB.open(databaseName, version);
 request.onerror = function (event) {
     console.log('数据库打开报错');
 };
-var db;
 request.onsuccess = function (event) {
   db = request.result;
   console.log('数据库打开成功');
+  // add data
+  add({
+    id: 1,
+    data: { id: 1, name: '张三', age: 24, email: 'zhangsan@example.com' },
+    db
+  });
+  add({
+    id: 2,
+    data: { id: 2, name: '张三2', age: 25, email: 'zhangsan2@example.com' },
+    db
+  });
+  // read data
+  read();
+  // read data
+  readAll();
+  update();
 };
-var db;
 request.onupgradeneeded = function (event) {
   db = event.target.result;
+  var objectStore = db.createObjectStore('person', { keyPath: 'id' });
+  // create index for name
+  objectStore.createIndex('name', 'name', { unique: false });
+  // create index for emali
+  objectStore.createIndex('email', 'email', { unique: true });
 }
 
-// upgrade version
-request.onupgradeneeded = function(event) {
-    db = event.target.result;
-    var objectStore = db.createObjectStore('person', { keyPath: 'id' });
+function add({
+  db,
+  id,
+  data
+}) {
+  var request = db.transaction(['person'], 'readwrite')
+    .objectStore('person')
+    .add({
+      id,
+      ...data
+    });
+
+  request.onsuccess = function (event) {
+    console.log('数据写入成功');
+  };
+
+  request.onerror = function (event) {
+    console.log('数据写入失败');
+  }
 }
-
-// create index
-request.onupgradeneeded = function(event) {
-    db = event.target.result;
-    var objectStore = db.createObjectStore('person', { keyPath: 'id' });
-    objectStore.createIndex('name', 'name', { unique: false });
-    objectStore.createIndex('email', 'email', { unique: true });
-  }
-
-
-
-  function add() {
-    var request = db.transaction(['person'], 'readwrite')
-      .objectStore('person')
-      .add({ id: 1, name: '张三', age: 24, email: 'zhangsan@example.com' });
-  
-    request.onsuccess = function (event) {
-      console.log('数据写入成功');
-    };
-  
-    request.onerror = function (event) {
-      console.log('数据写入失败');
-    }
-  }
-  
-  add();
-
-
 
 function read() {
    var transaction = db.transaction(['person']);
@@ -68,9 +74,6 @@ function read() {
    };
 }
 
-read();
-
-
 
 function readAll() {
   var objectStore = db.transaction('person').objectStore('person');
@@ -90,8 +93,6 @@ function readAll() {
   };
 }
 
-// read data
-readAll();
 function update() {
     var request = db.transaction(['person'], 'readwrite')
       .objectStore('person')
@@ -106,7 +107,6 @@ function update() {
     }
   }
   
-update();
 
 function remove() {
     var request = db.transaction(['person'], 'readwrite')
@@ -120,17 +120,20 @@ function remove() {
   
 // remove();
 
+function getByIndex() {
+  var transaction = db.transaction(['person'], 'readonly'); // 
+  var store = transaction.objectStore('person');
+  var index = store.index('name');
+  var request = index.get('李四');
 
-var transaction = db.transaction(['person'], 'readonly');
-var store = transaction.objectStore('person');
-var index = store.index('name');
-var request = index.get('李四');
-
-request.onsuccess = function (e) {
-  var result = e.target.result;
-  if (result) {
-    // ...
-  } else {
-    // ...
+  request.onsuccess = function (e) {
+    var result = e.target.result;
+    if (result) {
+      // ...
+    } else {
+      // ...
+    }
   }
 }
+
+new Promise(function (resolve, reject) { resolve('success') }).then((e) => { console.log(e) }, () => { })
