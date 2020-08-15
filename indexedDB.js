398d844919e32806b7fd7586a357e7662c302ab0
huaian <nopains@126.com>
2020-08-15 22:23:50 +0800

    [代码重构]添加promise接口

--- indexedDB.js
+++ indexedDB.js
@@ -45,33 +45,36 @@ function getInstance({
             tableName
           })
         },
-        read: ({
+        read: async ({
           id
         }) => {
-          read({
+          let result = await read({
             id,
             db,
             tableName
           })
+          return result;
         },
-        readAll: ({
+        readAll: async ({
         }) => {
-          readAll({
+          let result = await readAll({
             db,
             tableName
           })
+          return result;
         },
-        readByIndex: ({
+        readByIndex: async ({
           tableName,
           indexKey,
           indexValue
         }) => {
-          readByIndex({
+          let result = await readByIndex({
             db,
             indexKey,
             indexValue,
             tableName
           })
+          return result;
         }
       }
       resolve(instance);
@@ -83,7 +86,7 @@ getInstance({
   databaseName: 'webassembly',
   tableName: 'person',
   version: 1
-}).then((instance) => {
+}).then(async (instance) => {
   instance.add({
     id: 1,
     data: { id: 1, name: '张三', age: 24, email: 'zhangsan@example.com' }
@@ -93,15 +96,15 @@ getInstance({
     data: { id: 2, name: '张三2', age: 25, email: 'zhangsan2@example.com' }
   });
   // read data
-  instance.read({
+  let readResult = await instance.read({
     id: 1
   });
-  instance.read({
-    id: 2
-  });
+
+  console.log('readResult:', readResult);
   // // read data
-  readAll({
+  let readAllResult = await readAll({
   });
+  console.log('readAllResult:', readAllResult);
   instance.update({
     id: 1,
     data: {
@@ -110,10 +113,12 @@ getInstance({
       email: 'izhangsan@example.com'
     }
   });
-  instance.readByIndex({
+
+  let readByIndexResult = await instance.readByIndex({
     indexKey: 'name',
     indexValue: 'i张三'
   })
+  console.log('readIndexResult:', readByIndexResult);
 }, (err) => {
 })
 
@@ -149,49 +154,55 @@ function read({
   db,
   id
 }) {
-  var transaction = db.transaction([tableName]);
-  var objectStore = transaction.objectStore(tableName);
-  var request = objectStore.get(id);
+  return new Promise((resolve, reject) => {
+    var transaction = db.transaction([tableName]);
+    var objectStore = transaction.objectStore(tableName);
+    var request = objectStore.get(id);
+    request.onerror = function (event) {
+      console.log('事务失败');
+    };
 
-  request.onerror = function (event) {
-    console.log('事务失败');
-  };
-
-  request.onsuccess = function (event) {
-    if (request.result) {
-      console.log('Name: ' + request.result.name);
-      console.log('Age: ' + request.result.age);
-      console.log('Email: ' + request.result.email);
-    } else {
-      console.log('未获得数据记录');
-    }
-  };
+    request.onsuccess = function (event) {
+      if (request.result) {
+        console.log('Name: ' + request.result.name);
+        console.log('Age: ' + request.result.age);
+        console.log('Email: ' + request.result.email);
+        resolve(request.result);
+      } else {
+        console.log('未获得数据记录');
+      }
+    };
+  })
 }
 
 
 // read data list
 function readAll() {
-  var queryResult = [];
-  var objectStore = db.transaction('person').objectStore('person');
-  objectStore.openCursor().onsuccess = function (event) {
-    var cursor = event.target.result;
-    if (cursor) {
-      //  console.log('Id: ' + cursor.key);
-      //  console.log('Name: ' + cursor.value.name);
-      //  console.log('Age: ' + cursor.value.age);
-      //  console.log('Email: ' + cursor.value.email);
-      queryResult.push({
-        id: cursor.key,
-        name: cursor.value.name,
-        age: cursor.value.age,
-        Email: cursor.value.email
-      });
-      cursor.continue();
-    } else {
-      console.log(queryResult);
-      console.log('没有更多数据了！');
-    }
-  };
+  return new Promise((resolve, reject) => {
+    var queryResult = [];
+    var objectStore = db.transaction('person').objectStore('person');
+    objectStore.openCursor().onsuccess = function (event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        //  console.log('Id: ' + cursor.key);
+        //  console.log('Name: ' + cursor.value.name);
+        //  console.log('Age: ' + cursor.value.age);
+        //  console.log('Email: ' + cursor.value.email);
+        queryResult.push({
+          id: cursor.key,
+          name: cursor.value.name,
+          age: cursor.value.age,
+          Email: cursor.value.email
+        });
+        cursor.continue();
+      } else {
+        console.log(queryResult);
+        resolve(queryResult)
+        console.log('没有更多数据了！');
+      }
+    };
+
+  })
 }
 
 function update({
@@ -235,21 +246,24 @@ function readByIndex({
   indexKey,
   indexValue
 }) {
-  var transaction = db.transaction(['person'], 'readonly'); // 
-  var store = transaction.objectStore('person');
-  var index = store.index(indexKey);
-  var request = index.get(indexValue);
-
-  request.onsuccess = function (e) {
-    var result = e.target.result;
-    if (result) {
-      console.log('Name: ' + result.name);
-      console.log('Age: ' + result.age);
-      console.log('Email: ' + result.email);
-      // ...
-    } else {
-      // ...
+  return new Promise((resolve, reject) => {
+    var transaction = db.transaction(['person'], 'readonly'); // 
+    var store = transaction.objectStore('person');
+    var index = store.index(indexKey);
+    var request = index.get(indexValue);
+    request.onsuccess = function (e) {
+      var result = e.target.result;
+      if (result) {
+        console.log('Name: ' + result.name);
+        console.log('Age: ' + result.age);
+        console.log('Email: ' + result.email);
+        resolve(result);
+        // ...
+      } else {
+        // ...
+      }
     }
-  }
+
+  })
 }
 
