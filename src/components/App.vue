999912a4c1c80d27ee0a1df7d583750378e1e3ac
huaian <nopains@126.com>
2020-08-22 14:14:44 +0800

    add export import questions feature

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,6 +3,31 @@
     <!-- <div id="nav">
       <el-button>按钮</el-button>
     </div> -->
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="stem">
+        <el-input v-model="question.name"></el-input>
+      </el-form-item>
+      <el-form-item label="question">
+        <el-input v-model="question.question"></el-input>
+      </el-form-item>
+      <el-form-item label="option one">
+        <el-input v-model="question.input.options[0].label"></el-input>
+        <el-input v-model="question.input.options[0].value"></el-input>
+      </el-form-item>
+      <el-form-item label="option two">
+        <el-input v-model="question.input.options[1].label"></el-input>
+        <el-input v-model="question.input.options[1].value"></el-input>
+      </el-form-item>
+      <el-form-item label="option three">
+        <el-input v-model="question.input.options[2].label"></el-input>
+        <el-input v-model="question.input.options[2].value"></el-input>
+      </el-form-item>
+      <el-form-item label="option four">
+        <el-input v-model="question.input.options[3].label"></el-input>
+        <el-input v-model="question.input.options[3].value"></el-input>
+      </el-form-item>
+  </el-form>
+    <el-button @click="addItem">add</el-button>
     <input type="button" value="导入" id="import" />
     <input type="button" value="导出" id="export" onclick="exportAnswers()">
     <input type="button" value="导出questions" id="export" onclick="exportQuestions()">
@@ -26,11 +51,53 @@ import _ from 'lodash'
 // 基于准备好的dom，初始化echarts实例
 var echarts = require('echarts');
 import {
-    getQuestions
+    getQuestions, setQuestions
 } from '../utils/questions'
 export default {
   name: 'App',
+  data() {
+    return {
+      questions: [],
+      question: {
+        type: "SingleChoice",
+        stem: ["Some animals are very big; some are small"],
+        question: ["what is  the biggest animal"],
+        input: {
+            options: [
+                {
+                    value: 'A',
+                    label: '黄金糕'
+                },
+                {
+                    value: 'B',
+                    label: 'choclate'
+                },
+                {
+                    value: 'C',
+                    label: 'choclate'
+                },
+                {
+                    value: 'C',
+                    label: 'choclate'
+                }
+            ]
+        },
+        score: 5,
+        correctAnswer: "A",
+        answer: '',
+        extend: {}
+    },
+    }
+  },
   methods: {
+    addItem () {
+      this.questions.push(this.question)
+      console.log(this.question)
+      console.log(this.questions)
+      setQuestions({
+        questions: this.questions
+      })
+    },
     async getSum () {
       let questions = await getQuestions()
       console.log(questions)
